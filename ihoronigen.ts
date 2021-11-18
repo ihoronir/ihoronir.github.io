b2e5931dc2a8eecdd7b956bfd992eb5bef88a7e0
ihoronir <20489316+ihoronir@users.noreply.github.com>
2021-11-18 22:57:26 +0900

    update

--- ihoronigen.ts
+++ ihoronigen.ts
@@ -14,17 +14,34 @@
 // 
 
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
-import katex from 'https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.mjs';
+import katex from "https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.mjs";
 
 const addr = ":8080";
+
 console.log(`KaTeX compile server listening on http://localhost${addr}
 `);
 
 serve((request: Request) => {
     const { pathname } = new URL(request.url);
-    const html = katex.renderToString(decodeURIComponent(pathname).slice(1), {
-        displayMode: true
+	const data = decodeURIComponent(pathname).slice(1);
+
+	let source = "";
+	let displayMode = false;
+
+	if (data.startsWith("block")) {
+		source = data.slice("block".length);
+		displayMode = true
+	}
+
+	if (data.startsWith("inline")) {
+		source = data.slice("inline".length);
+		displayMode = false;
+	}
+
+    const html = katex.renderToString(source, {
+        displayMode
     });
+
     return new Response(html);
 }, { addr });
 
