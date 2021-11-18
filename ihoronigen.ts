f2b95ba3b4dd89e92cc5ac6cede5dea0288dd9e1
ihoronir <20489316+ihoronir@users.noreply.github.com>
2021-11-18 20:57:31 +0900

    update

--- ihoronigen.ts
+++ ihoronigen.ts
@@ -1,8 +1,24 @@
+// # Installation
+//
+// $ deno install --allow-run --allow-net ihoronigen.ts
+//
+// # Usage
+//
+// ## Serve
+//
+// $ ihoronigen serve
+//
+// ## Build
+//
+// $ ihoronigen build
+// 
+
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 import katex from 'https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.mjs';
 
 const addr = ":8080";
-console.log(`HTTP server listening on http://localhost${addr}`);
+console.log(`KaTeX compile server listening on http://localhost${addr}
+`);
 
 serve((request: Request) => {
     const { pathname } = new URL(request.url);
@@ -12,7 +28,7 @@ serve((request: Request) => {
     return new Response(html);
 }, { addr });
 
-await Deno.run({ cmd: ["zola", "build"] }).status();
+await Deno.run({ cmd: ["zola", Deno.args[0]] }).status();
 
 console.log("END");
 
